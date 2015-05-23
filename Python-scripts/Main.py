1b1f898e4305557e08829105b817b89fa97cc55f
naunauyoh <arnaudyoh@gmail.com>
2015-05-23 18:49:43 -0400

    Working result giver

--- Python-scripts/Main.py
+++ Python-scripts/Main.py
@@ -11,25 +11,37 @@ class City :
         self.Name = Name
         self.Tags = Tags
 
+class Result :
+    def __init__(self,Name,Points,Price,Outgoing,Returning,url):
+        self.Name = Name
+        self.Points = Points
+        self.Price = Price
+        self.Outgoing = Outgoing
+        self.Returning = Retunring
+        self.url = url
+        
 def rankCities(listOfCities, listofactivities, startdate, enddate):
-	apikey = '&apikey=j3HAAFcwQG6n3X1Q0Ec84wgwZuDmwiFY'
-	url = 'http://terminal2.expedia.com:80/x/activities/search?'
-	for city in listOfCities:
-		url = url+'&location=' + str(city.Name)
-		if(startdate):
-			url = url + '&startDate'+startdate
-			if(enddate):
-				url = url+'&endDate'+enddate
-		url = url + apikey
-		response = urllib2.urlopen(url).read()
-		data = json.loads(response)
-		for activity in listofactivities:
-			try:
-				city.Points = city.Points + data['filterCategories'][activity]['count']
-			except:
-				continue
+    for city in listOfCities :
+        apikey = '&apikey=j3HAAFcwQG6n3X1Q0Ec84wgwZuDmwiFY'
+        url = 'http://terminal2.expedia.com:80/x/activities/search?'
+        url = url+'&location=' + str(city.Name)
+        if(startdate):
+            url = url + '&startDate'+startdate
+            if(enddate):
+                url = url+'&endDate'+enddate
+	url = url + apikey
+        response = urllib2.urlopen(url).read()
+        print url
+	data = json.loads(response)
+	for activity in listofactivities:
+	    try:
+                city.Points = city.Points + data['filterCategories'][activity]['count']
+	    except:
+	       continue
+    return sorted(listOfCities, key=lambda city: city.Points, reverse=True)
 
 Cities = []
+Results = []
 
 Destination = ["Berlin","London","Montreal","Dubai","Phuket"]
 BerTags =["Big","Historical", "Sausage", "CurryWurst","NightLife","German","Brandenburg","Reichstag","Germany"]
@@ -44,11 +56,12 @@ for i in range(len(Destination)) :
     Cities.append(city)
 
 form = cgi.FieldStorage()
-BegDate = ""
-EndDate = ""
+BegDate = "2015-06-10"
+EndDate = "2015-06-12"
 CatChoice = ""
-RequestTags = ["German"]
-RequestActivities = ["Food & Drinks","Nightlife"]
+RequestTags = ["Historical"]
+RequestActivities = ["Nightlife"]
+Departure = "Montreal"
 if "startDate" in form:
     Begdate=form["startDate"].value
     if(form["EndDate"].value) :   
@@ -59,6 +72,8 @@ if "Tags" in form :
     RequestTags = form["Tags"].value
 if "Activities" in form :
     RequestActivities = form["Activities"].value
+if "Departure" in form:
+    Departure = form["Departure"].value
 NarrowDest =[]
 for i in range(len(RequestTags)):
     for j in range(len(Cities)):
@@ -68,6 +83,36 @@ for i in range(len(RequestTags)):
                 
                 NarrowDest.append(Cities[j])
 
-rankCities(NarrowDest, RequestActivities, BegDate, EndDate)
+RankedList = rankCities(NarrowDest, RequestActivities, BegDate, EndDate)
 
-print NarrowDest[0].Name +" :"+ str(NarrowDest[0].Points) + " Points"
+for city in RankedList :
+    DepCity = Departure.lower()
+    ArrCity = city.lower()
+    DepCity.replace(" ", "%20")
+    ArrCity.replace(" ", "%20")
+
+    DepUrl = "http://terminal2.expedia.com/x/suggestions/regions?query="+DepCity+"&apikey=bWi3OmhFA3L34BLYkjYsucA6SVmMU8lr"
+    response = urllib2.urlopen(DepUrl).read()
+    data = json.loads(response)
+    DepAir = data["sr"][0]["a"]
+
+    ArrUrl = "http://terminal2.expedia.com/x/suggestions/regions?query="+ArrCity+"&apikey=bWi3OmhFA3L34BLYkjYsucA6SVmMU8lr"
+    response = urllib2.urlopen(ArrUrl).read()
+    data = json.loads(response)
+    ArrID = data['sr'][0]['id']
+    ArrAir = data['sr'][0]['a']
+
+
+    PackageUrl = "http://terminal2.expedia.com:80/x/packages?originAirport="+DepAir+"&destinationAirport="+ArrAir+"&departureDate="+BegDate+"&returnDate="+EndDate+"&regionid="+ArrID+"&limit=1&apikey=bWi3OmhFA3L34BLYkjYsucA6SVmMU8lr"
+    response = urllib2.urlopen(PackageUrl).read()
+    data = json.loads(response)
+    DepTravelTime =data["FlightList"]["Flight"]["FlightItinerary"]["FlightLeg"][0]["FlightDuration"]
+    RetTravelTime =data["FlightList"]["Flight"]["FlightItinerary"]["FlightLeg"][1]["FlightDuration"]
+    Price = str(data["PackageSearchResultList"]["PackageSearchResult"]["PackagePrice"]["TotalPrice"]["Value"])+" "+data["PackageSearchResultList"]["PackageSearchResult"]["PackagePrice"]["TotalPrice"]["Currency"]
+    ResultUrl = data["PackageSearchResultList"]["PackageSearchResult"]["DetailsUrl"]
+
+    DepTravelTime = DepTravelTime.replace("PT","").replace("H"," hours and ").replace("M"," minutes") 
+    RetTravelTime = RetTravelTime.replace("PT","").replace("H"," hours and ").replace("M"," minutes") 
+    result = Result(city.Name,city.Points,Price,DepTravelTime,RetTravelTime,Resulturl)
+    Results.append(result)
+    
