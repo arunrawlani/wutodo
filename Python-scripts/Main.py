7922fb1814fabb59e160d5da1f393e1a12f0c0c4
naunauyoh <arnaudyoh@gmail.com>
2015-05-23 14:43:54 -0400

    Final working python

--- Python-scripts/Main.py
+++ Python-scripts/Main.py
@@ -11,6 +11,24 @@ class City :
         self.Name = Name
         self.Tags = Tags
 
+def rankCities(listOfCities, listofactivities, startdate, enddate):
+	apikey = '&apikey=j3HAAFcwQG6n3X1Q0Ec84wgwZuDmwiFY'
+	url = 'http://terminal2.expedia.com:80/x/activities/search?'
+	for city in listOfCities:
+		url = url+'&location=' + str(city.Name)
+		if(startdate):
+			url = url + '&startDate'+startdate
+			if(enddate):
+				url = url+'&endDate'+enddate
+		url = url + apikey
+		response = urllib2.urlopen(url).read()
+		data = json.loads(response)
+		for activity in listofactivities:
+			try:
+				city.Points = city.Points + data['filterCategories'][activity]['count']
+			except:
+				continue
+
 Cities = []
 
 Destination = ["Berlin","London","Montreal","Dubai","Phuket"]
@@ -30,6 +48,7 @@ BegDate = ""
 EndDate = ""
 CatChoice = ""
 RequestTags = ["German"]
+RequestActivities = ["Food & Drinks","Nightlife"]
 if "startDate" in form:
     Begdate=form["startDate"].value
     if(form["EndDate"].value) :   
@@ -38,17 +57,17 @@ if "Type of Trip" in form :
     CatChoice = form["Type of trip"].value
 if "Tags" in form : 
     RequestTags = form["Tags"].value
-
+if "Activities" in form :
+    RequestActivities = form["Activities"].value
 NarrowDest =[]
 for i in range(len(RequestTags)):
     for j in range(len(Cities)):
         for k in range(len(Cities[j].Tags)):
-            print Cities[j].Tags[k]
             
             if RequestTags[i] == Cities[j].Tags[k]:
                 
                 NarrowDest.append(Cities[j])
-        print "\n"
 
-for i in range(len(NarrowDest)) :
-    print NarrowDest[i].Name
+rankCities(NarrowDest, RequestActivities, BegDate, EndDate)
+
+print NarrowDest[0].Name +" :"+ str(NarrowDest[0].Points) + " Points"
